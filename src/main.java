a277ce9333959c3f2cd04fdc3a3af3e7314e3675
jakub <jaku0584@stud.kea.dk>
2017-02-28 09:13:22 +0100

    pls work!

---
+++ src/main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Jakub Petrík on 28/02/2017.
+ */
+public class main {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
